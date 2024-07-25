003cfe3d5cc27914167dc3a2eb9a51db2cb7cc96
michealagina <ebubechukwu.agina@binghamuni.edu.ng>
2024-07-25 18:32:59 +0100

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -2,7 +2,7 @@ package Java2;
 
 public class Variables {
     public static void main(String[] args) {
-        String name = "Gabriel";
+        String name = "Ebubechukwu";
         boolean alive = true;
         if (alive == true){
             System.out.println(name);
