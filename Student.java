467c37a5808f5f6be7479aebfc91248008681288
michealagina <ebubechukwu.agina@binghamuni.edu.ng>
2024-07-25 18:31:13 +0100

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,8 @@
 package User;
 
 public class Student extends User {
-    String matric_no = "bhu/22/04/05/0102";
-    String admission_no = "302245652237";
+    String matric_no = "bhu/22/04/09/0059";
+    String admission_no = "302283008547";
     String registerCourse() {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "You cannot register at this moment";
@@ -14,13 +14,13 @@ public class Student extends User {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "Cannot check your result";
         } else {
-            return "You got 6As and 1B";
+            return "You got 5As and 2B";
         }
     }
     public static void main(String[] args) {
         Student student = new Student();
-        student.name = "Onuma Gabriel";
-        student.password = "gabriel2004";
+        student.name = "Agina Ebubechukwu ";
+        student.password = "micheal2003";
         System.out.println(student.register());
         System.out.println(student.registerCourse());
         System.out.println(student.checkResults());
